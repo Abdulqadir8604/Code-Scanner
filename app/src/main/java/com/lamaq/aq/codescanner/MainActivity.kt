715730be67698a3bc1e96d7304dc94cf04be7e65
Abdulqadir8604 <web.aq8604@gmail.com>
2023-11-01 13:57:32 +0530

    first commit

--- app/src/main/java/com/lamaq/aq/codescanner/MainActivity.kt
+++ app/src/main/java/com/lamaq/aq/codescanner/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lamaq.aq.codescanner
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -12,10 +14,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.selection.selectable
@@ -30,7 +34,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,10 +41,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
@@ -49,17 +49,19 @@ import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import com.lamaq.aq.codescanner.ui.theme.CodeScannerTheme
 import kotlinx.coroutines.launch
 
+
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             CodeScannerTheme {
                 val scope = rememberCoroutineScope()
                 val snackbarHostState = remember { SnackbarHostState() }
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
                     color = MaterialTheme.colorScheme.primary,
                 ) {
                     val options = GmsBarcodeScannerOptions.Builder()
@@ -83,8 +85,17 @@ class MainActivity : ComponentActivity() {
                                 )
                             }
                         },
-                        modifier = Modifier.fillMaxSize(),
                         content = {
+                            Row {
+                                Text(
+                                    text = "Code Scanner",
+                                    style = MaterialTheme.typography.headlineLarge,
+                                    modifier = Modifier
+                                        .align(Alignment.CenterVertically)
+                                        .padding(10.dp)
+
+                                )
+                            }
                             Column(
                                 modifier = Modifier.fillMaxSize(),
                                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -122,11 +133,23 @@ class MainActivity : ComponentActivity() {
                                                     .selectable(
                                                         selected = true,
                                                         onClick = {
-                                                            Toast.makeText(
-                                                                this@MainActivity,
-                                                                "Copied to Clipboard",
-                                                                Toast.LENGTH_SHORT
-                                                            ).show()
+                                                            val clipboard: ClipboardManager =
+                                                                getSystemService(
+                                                                    CLIPBOARD_SERVICE
+                                                                ) as ClipboardManager
+                                                            val clip =
+                                                                ClipData.newPlainText(
+                                                                    rawValue,
+                                                                    rawValue
+                                                                )
+                                                            clipboard.setPrimaryClip(clip)
+                                                            Toast
+                                                                .makeText(
+                                                                    this@MainActivity,
+                                                                    "Copied to Clipboard",
+                                                                    Toast.LENGTH_SHORT
+                                                                )
+                                                                .show()
                                                         }
                                                     ),
                                             )
@@ -136,6 +159,7 @@ class MainActivity : ComponentActivity() {
                                             modifier = Modifier
                                                 .align(Alignment.CenterHorizontally),
                                             onClick = {
+                                                rawValue = "Loading..."
                                             scanner
                                                 .startScan()
                                                 .addOnSuccessListener { barcode ->
@@ -181,13 +205,15 @@ class MainActivity : ComponentActivity() {
                                                     }
                                                 }
                                                 .addOnCanceledListener {
+                                                    rawValue = "Task Cancelled"
                                                     scope.launch {
-                                                        snackbarHostState.showSnackbar("Task Cancelled")
+                                                        snackbarHostState.showSnackbar("Task Cancelled by User")
                                                     }
                                                 }
                                                 .addOnFailureListener { e ->
+                                                    rawValue = "Task Failed"
                                                     scope.launch {
-                                                        snackbarHostState.showSnackbar("Task Failed")
+                                                        snackbarHostState.showSnackbar("Task Failed: ${e.message}")
                                                     }
                                                 }
                                         }) {
@@ -202,7 +228,9 @@ class MainActivity : ComponentActivity() {
                                             modifier = Modifier
                                                 .align(Alignment.CenterHorizontally),
                                             onClick = {
-                                                rawValue = "Feature Coming Soon..."
+                                                rawValue = "Loading..."
+                                                val intent = Intent(this@MainActivity, CodeGen::class.java)
+                                                startActivity(intent)
                                             }) {
                                             Text(
                                                 text = "Generate Code",
